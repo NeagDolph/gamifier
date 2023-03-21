33767b0500e0173bc6fd4e107777475e371ce466
Neil Agrawal <neilagrawal@icloud.com>
2023-03-20 20:03:25 -0700

    first commit

---
+++ run.js
@@ -0,0 +1,14 @@
+// Minimum length (minimum_length)
+// 500 words
+
+// Story Idea (story_plot)
+// A funny youtube video sketch type video where characters voice over a meta and self-humorous dialogue over a videogame gameplay in the background. Ellen spertus a computer science teacher gets mad Rasika the TA because they're stuck in a youtube video on top of minecraft gameplay (make it super meta)
+
+// Story characteristics (story_keywords)
+// Meta, funny, they bash each other, comedic, self-depricating, amusing, funny, hilarious.
+
+//Take this info and using the plot_creation_script and chatGPT create the script for the story: story_plot
+
+// Take the story_plot and create a story dialogue using story_creation_script
+
+
